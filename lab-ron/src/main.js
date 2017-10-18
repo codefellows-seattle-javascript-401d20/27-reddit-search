698bf543ec32c880ba3854def9f8547e03e152fa
Ron Barrantes <ronbarrantes@gmail.com>
2017-10-18 01:48:38 -0700

    pulling things from reddit

--- lab-ron/src/main.js
+++ lab-ron/src/main.js
@@ -5,6 +5,9 @@ import superagent from 'superagent'
 
 const apiURL = 'https://www.reddit.com/r/programming.json'
 
+let searchFormBoard = 'aww'
+let searchFormLimit = 10
+
 class Header extends React.Component {
   render() {
     return (
@@ -15,37 +18,50 @@ class Header extends React.Component {
   }
 }
 
-
-
-
-
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      redditArticles: [],
+      redditBoard: [],
     }
   }
 
   componentDidMount() {
     console.log('will mount')
-    superagent.get('https://www.reddit.com/r/programming.json')
+    superagent.get(`https://www.reddit.com/r/${searchFormBoard}.json?limit=${searchFormLimit}`)
+      // .send({'limit': 100})
       .then(res => {
-        if (res.body.results)
-          this.setState({redditArticles: res.body.results})
+        if (res.body)
+          this.setState({ redditBoard: res.body.data.children.map(post => post.data) })
       })
       .catch(console.error)
   }
 
-  componentDidUpdate(){
-    console.log('STATE: ', this.state)
-  }
+  componentDidUpdate() {
 
+    // const { author, title, url } = this.state.redditBoard
+    console.log('STATE: ', this.state.redditBoard)
+    // console.log(author, title, url)
+  }
 
   render() {
     return (
       <div>
         <Header />
+        <ul>
+          {this.state.redditBoard.map((post, i) => {
+            let { author, title, url, selftext_html } = post
+            let image = post.preview.images[0].source.url
+            return <li>
+              {author}<br />
+              <a href={url} target="_blank" >{title}</a>
+              <p>{selftext_html}</p>
+
+  
+              <img src={image} />
+            </li>
+          })}
+        </ul>
       </div>
     )
   }
