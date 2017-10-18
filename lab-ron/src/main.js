57ca04933b0841586606d6b1324e3cbc703c2e63
Ron Barrantes <ronbarrantes@gmail.com>
2017-10-18 00:39:39 -0700

    trying to get superagent to wrk

--- lab-ron/src/main.js
+++ lab-ron/src/main.js
@@ -3,6 +3,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import superagent from 'superagent'
 
+const apiURL = 'https://www.reddit.com/r/programming.json'
+
 class Header extends React.Component {
   render() {
     return (
@@ -13,11 +15,37 @@ class Header extends React.Component {
   }
 }
 
+
+
+
+
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      redditArticles: [],
+    }
+  }
+
+  componentDidMount() {
+    console.log('will mount')
+    superagent.get('https://www.reddit.com/r/programming.json')
+      .then(res => {
+        if (res.body.results)
+          this.setState({redditArticles: res.body.results})
+      })
+      .catch(console.error)
+  }
+
+  componentDidUpdate(){
+    console.log('STATE: ', this.state)
+  }
+
+
   render() {
     return (
       <div>
-        <Header/>
+        <Header />
       </div>
     )
   }
