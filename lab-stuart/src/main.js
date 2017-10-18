75113cf843b1306a1b28f4266190a7501bf90d5b
Stuart Kershaw <stuartdkershaw@gmail.com>
2017-10-18 08:18:27 -0700

    App and Search Form

--- lab-stuart/src/main.js
+++ lab-stuart/src/main.js
@@ -1,3 +1,95 @@
+import './style/main.scss';
+
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDom from 'react-dom';
 import superagent from 'superagent';
+
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      board: 'programming',
+      limit: 10,
+      topics: []
+    }
+
+    this.handleBoard = this.handleBoard.bind(this);
+    this.handleLimit = this.handleLimit.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleBoard(e) {
+    let {value} = e.target;
+    this.setState(() => {
+      return {
+        board: value
+       };
+    });
+  }
+
+  handleLimit(e) {
+    let {value} = e.target;
+    this.setState(() => {
+      return {
+        limit: value
+       };
+    });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    superagent.get(`https://reddit.com/r/${this.state.board}.json?limit=${this.state.limit}`)
+    .then(res => {
+      if(res.body.results)
+        this.setState({ topics: res.body.results });
+    })
+    .catch(console.error);
+  }
+
+  componentDidUpdate(){
+    console.log('__STATE__', this.state)
+  }
+
+  render(){
+    return (
+      <SearchForm
+        board={this.state.board}
+        limit={this.state.limit}
+        handleBoard={this.handleBoard}
+        handleLimit={this.handleLimit}
+        handleSubmit={this.handleSubmit}
+      />
+    )
+  }
+
+}
+
+class SearchForm extends React.Component {
+  constructor(props){
+    super(props)
+  }
+
+  render(){
+    return (
+      <form onSubmit={this.props.handleSubmit}>
+        <input
+          type="text"
+          value={this.props.board}
+          onChange={this.props.handleBoard}
+        />
+        <br />
+        <input
+          type="number"
+          value={this.props.limit}
+          onChange={this.props.handleLimit}
+        />
+        <br />
+        <input type="submit" value="Submit" />
+      </form>
+    )
+  }
+}
+
+let container = document.createElement('div')
+document.body.appendChild(container)
+ReactDom.render(<App />, container)
