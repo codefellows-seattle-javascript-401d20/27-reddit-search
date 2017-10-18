6468dd9a341ef20f4f6f7a35b93606bfbf22297f
Christina <penssake@gmail.com>
2017-10-18 00:23:13 -0700

    main.js partial submit

--- lab-christina/src/main.js
+++ lab-christina/src/main.js
@@ -7,18 +7,42 @@ import ReactDom from 'react-dom'
 import superagent from 'superagent'
 import cors from 'cors'
 
+let searchFormBoard = 'hot'
+let searchFormLimit = 4
+
 class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      redditBoard: [],
+    }
+  }
+
   componentWillMount() {
     console.log('will mount')
-    superagent.get(`http://reddit.com/r/${'hot'}.json?limit=${4}`)
+    superagent.get(`http://reddit.com/r/${searchFormBoard}.json?limit=${searchFormLimit}`)
       .then(response => {
-        console.log(response)
+        console.log({response})
+        let data = response
+        if(data)
+          this.setState({redditBoard: this.body.data})
       })
+      .catch(console.error)
   }
+
+  componenetDidUpdate() {
+    console.log('__STATE__::', this.state)
+  }
+
   render() {
     return (
       <div>
         <h1> Reddit Search </h1>
+        <ul>
+          {this.state.redditBoard.map((redditBoard, i) => {
+
+          })}
+        </ul>
       </div>
     )
   }
