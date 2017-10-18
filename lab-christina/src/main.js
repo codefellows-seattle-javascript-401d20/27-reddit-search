e51754dee87f3e287e416c82371ac78f452d2cb1
Christina <penssake@gmail.com>
2017-10-17 23:19:23 -0700

    scaffolding / cors / cross origin probs

--- lab-christina/src/main.js
+++ lab-christina/src/main.js
@@ -1,37 +1,27 @@
 'use strict'
 
+import './style/main.scss'
+
 import React from 'react'
 import ReactDom from 'react-dom'
 import superagent from 'superagent'
+import cors from 'cors'
 
 class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      //topics array rom result of the search
-    }
-    //bind my methods of my Component
+  componentWillMount() {
+    console.log('will mount')
+    superagent.get(`http://reddit.com/r/${'hot'}.json?limit=${4}`)
+      .then(response => {
+        console.log(response)
+      })
   }
-
-  handleChange(e){
-    this.setState({result: e.target.value})
-  }
-
-  handleSubmit(e){
-    e.preventDefault()
-    this.props.nextSearchResult(this.props.redditSearch.result, this.state.result)
-  }
-
-  render(){
+  render() {
     return (
-      <form onSubmit={this.handleSubmit} >
-        <input
-          type='text'
-          name='searchResults'
-          value={this.state.result}
-          onChange={this.handleChange}
-        />
-      </form>
+      <div>
+        <h1> Reddit Search </h1>
+      </div>
     )
   }
 }
+
+ReactDom.render(<App />, root)
